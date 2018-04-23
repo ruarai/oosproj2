302f349723772a10c6460e2c936dd0a0083906c4
ruarai <ruarai.big@gmail.com>
2018-04-23 13:45:38 +1000

    refactor Player

--- src/Player.java
+++ src/Player.java
@@ -11,6 +11,8 @@ class Player extends Sprite {
     private static final float FRICTION_SCALE = 0.01f;
     private static final float DRIFT_SCALE = 1.7f;
 
+    private static final float DIR_FORWARDS = -90;
+
     private static final int SHIELD_OFFSET = 16;
 
     private static final int SHOT_DELAY = 250;
@@ -20,24 +22,28 @@ class Player extends Sprite {
     //indicates number of ms left until player can shoot a laser again
     private int shotDelay = 0;
 
-    @Override
     public void update(Input input, int delta) {
         move(input, delta);
 
         keepOnScreen();
 
+        tryShoot(input, delta);
+    }
+
+    private void tryShoot(Input input, int delta){
         //See if we need to shoot a laser
         //Use isKeyPressed to ensure 1 laser per key press
         if(input.isKeyDown(Input.KEY_SPACE) && shotDelay <= 0)
         {
             //So shoot a laser!
-
             Laser laser = new Laser(Resources.shot,this.getCentre(),parentWorld, rotation);
             parentWorld.addEntity(laser);
 
-            Vector2f recoil = new Vector2f(rotation - 90).scale(-RECOIL_ACCEL * delta);
+            //Calculate some amount of recoil
+            Vector2f recoil = new Vector2f(rotation + DIR_FORWARDS).scale(-RECOIL_ACCEL * delta);
             velocity.add(recoil);
 
+            //Reset the delay to SHOT_DELAY
             shotDelay = SHOT_DELAY;
         }
 
@@ -71,9 +77,9 @@ class Player extends Sprite {
 
         //respond to key inputs
         if(input.isKeyDown(Input.KEY_UP))
-            thrust = new Vector2f(rotation - 90).scale(MOVE_ACCEL * delta);
+            thrust = new Vector2f(rotation  + DIR_FORWARDS).scale(MOVE_ACCEL * delta);
         if(input.isKeyDown(Input.KEY_DOWN))
-            thrust = new Vector2f(rotation - 90).scale(-MOVE_ACCEL * delta);
+            thrust = new Vector2f(rotation  + DIR_FORWARDS).scale(-MOVE_ACCEL * delta);
         if(input.isKeyDown(Input.KEY_LEFT))
             rotation += -ROTATION_SPEED * delta * rotationScale;
         if(input.isKeyDown(Input.KEY_RIGHT))
@@ -81,7 +87,7 @@ class Player extends Sprite {
 
 
         Vector2f drift = new Vector2f();
-        velocity.projectOntoUnit(new Vector2f(rotation - 90),drift);
+        velocity.projectOntoUnit(new Vector2f(rotation + DIR_FORWARDS),drift);
         drift.sub(velocity);
         drift.scale(DRIFT_SCALE);
 
@@ -106,11 +112,6 @@ class Player extends Sprite {
 
         if(location.y < 0)
             location.y = 0;
-    }
-
-    public void onDeath()
-    {
-
     }
 
 }
