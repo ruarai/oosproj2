d36419cfd1047d01c2481475140466ee5643a059
ruarai <ruarai.big@gmail.com>
2018-05-07 17:16:24 +1000

    comment sine

--- src/EnemySine.java
+++ src/EnemySine.java
@@ -9,12 +9,16 @@ public class EnemySine extends Enemy {
     private static final float AMPLITUDE = 96f;
     private static final float PERIOD = 1500f;
 
+    //Line of y = initialX that our sine function will oscillate over
     private float initialX;
+    //Time elapsed since spawn to change sine value
     private float timeElapsed = 0;
 
     public EnemySine(Vector2f location, World parent) {
 
         super(Resources.sineEnemy, location, parent);
+
+        //Take note of our initial x location for use in our sine function
         initialX = location.x;
     }
 
@@ -22,13 +26,17 @@ public class EnemySine extends Enemy {
     public void update(Input input, int delta) {
         super.update(input, delta);
 
+        //increment the amount of time that has passed for use in our sine function
         timeElapsed += delta;
 
+        //Determine our basic velocity downwards
         Vector2f velocity = new Vector2f(DIRECTION_STRAIGHT);
         velocity.scale(SPEED * delta);
 
+        //Add this basic velocity
         getLocation().add(velocity);
 
+        //Move in the x direction according to our sine function
         getLocation().x = initialX + AMPLITUDE * (float)Math.sin(((2*Math.PI)/(PERIOD))*timeElapsed);
 
 
