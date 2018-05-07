9c5bad8e2b9e05f2f092e5fde895c29f2a2870bf
ruarai <ruarai.big@gmail.com>
2018-05-07 16:59:03 +1000

    Collidable commenting

--- src/Collidable.java
+++ src/Collidable.java
@@ -1,4 +1,5 @@
 //Interface for Sprites to implement that allows them to handle collision logic
 public interface Collidable {
+    //Method that handles this implementing Sprite colliding with some other Collidable Sprite
     void onCollision(Sprite sprite);
 }
